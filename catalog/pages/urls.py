de96b3e0290c70d7ffae11aa2554e7b278e44f4e
Atakan Karaçalı <atakankaracali@users.noreply.github.com>
2022-03-24 15:28:03 +0300

    a
    a

---
+++ catalog/pages/urls.py
@@ -0,0 +1,9 @@
+from importlib.resources import path
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+]
